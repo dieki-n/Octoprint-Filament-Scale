cc84db42d837d66d3eca8984a9465cc02b77638d
Leon Wright <techman83@gmail.com>
2021-03-10 08:07:12 +0800

    Line endings, whitespace, and tabs > spaces (PEP8)

--- octoprint_filament_scale/__init__.py
+++ octoprint_filament_scale/__init__.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 from __future__ import absolute_import
 import sys
-
+import statistics
 
 import octoprint.plugin
 import flask
@@ -10,73 +10,73 @@ from .hx711 import HX711
 import RPi.GPIO as GPIO
 
 class Filament_scalePlugin(octoprint.plugin.SettingsPlugin,
-						   octoprint.plugin.AssetPlugin,
-						   octoprint.plugin.TemplatePlugin,
-						   octoprint.plugin.StartupPlugin):
+                           octoprint.plugin.AssetPlugin,
+                           octoprint.plugin.TemplatePlugin,
+                           octoprint.plugin.StartupPlugin):
 
 
-	def get_template_configs(self):
-		return [
-			dict(type="settings", custom_bindings=True)
-		]
-	
-	def get_settings_defaults(self):
-		return dict(
-			tare = 8430152,
-			reference_unit=-411,
-			spool_weight=200,
-			clockpin=21,
-			datapin=20,
-			lastknownweight=0
-			
-			# put your plugin's default settings here
-		)
+    def get_template_configs(self):
+        return [
+            dict(type="settings", custom_bindings=True)
+        ]
+    
+    def get_settings_defaults(self):
+        return dict(
+            tare = 8430152,
+            reference_unit=-411,
+            spool_weight=200,
+            clockpin=21,
+            datapin=20,
+            lastknownweight=0
+            
+            # put your plugin's default settings here
+        )
 
-	##~~ AssetPlugin mixin
+    ##~~ AssetPlugin mixin
 
-	def get_assets(self):
-		# Define your plugin's asset files to automatically include in the
-		# core UI here.
-		return dict(
-			js=["js/filament_scale.js"],
-			css=["css/filament_scale.css"],
-			less=["less/filament_scale.less"]
-		)
+    def get_assets(self):
+        # Define your plugin's asset files to automatically include in the
+        # core UI here.
+        return dict(
+            js=["js/filament_scale.js"],
+            css=["css/filament_scale.css"],
+            less=["less/filament_scale.less"]
+        )
 
-	
-	def on_startup(self, host, port):
-		self.hx = HX711(20, 21)
-		self.hx.set_reading_format("LSB", "MSB") 
-		self.hx.reset()
-		self.hx.power_up()
-		self.t = octoprint.util.RepeatedTimer(3.0, self.check_weight)
-		self.t.start()
-		
-	def check_weight(self):
-		self.hx.power_up()
-		v = self.hx.read()
-		self._plugin_manager.send_plugin_message(self._identifier, v) 
-		self.hx.power_down()
-		
-	def get_update_information(self):
-		# Define the configuration for your plugin to use with the Software Update
-		# Plugin here. See https://github.com/foosel/OctoPrint/wiki/Plugin:-Software-Update
-		# for details.
-		return dict(
-			filament_scale=dict(
-				displayName="Filament Scale Plugin",
-				displayVersion=self._plugin_version,
+    
+    def on_startup(self, host, port):
+        self.hx = HX711(20, 21)
+        self.hx.set_reading_format("LSB", "MSB") 
+        self.hx.reset()
+        self.hx.power_up()
+        self.t = octoprint.util.RepeatedTimer(3.0, self.check_weight)
+        self.t.start()
+        
+    def check_weight(self):
+        self.hx.power_up()
+        v = self.hx.read()
+        self._plugin_manager.send_plugin_message(self._identifier, v) 
+        self.hx.power_down()
+        
+    def get_update_information(self):
+        # Define the configuration for your plugin to use with the Software Update
+        # Plugin here. See https://github.com/foosel/OctoPrint/wiki/Plugin:-Software-Update
+        # for details.
+        return dict(
+            filament_scale=dict(
+                displayName="Filament Scale Plugin",
+                displayVersion=self._plugin_version,
 
-				# version check: github repository
-				type="github_release",
-				user="dieki-n",
-				repo="OctoPrint-Filament-scale",
-				current=self._plugin_version,
+                # version check: github repository
+                type="github_release",
+                user="dieki-n",
+                repo="OctoPrint-Filament-scale",
+                current=self._plugin_version,
 
-				# update method: pip
-				pip="https://github.com/dieki-n/OctoPrint-Filament-scale/archive/{target_version}.zip"
-			)
-		)
+                # update method: pip
+                pip="https://github.com/dieki-n/OctoPrint-Filament-scale/archive/{target_version}.zip"
+            )
+        )
 
 
 # If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
@@ -86,11 +86,10 @@ __plugin_name__ = "Filament Scale"
 __plugin_pythoncompat__ = ">=3,<4"
 
 def __plugin_load__():
-	global __plugin_implementation__
-	__plugin_implementation__ = Filament_scalePlugin()
+    global __plugin_implementation__
+    __plugin_implementation__ = Filament_scalePlugin()
 
-	global __plugin_hooks__
-	__plugin_hooks__ = {
-		"octoprint.plugin5.softwareupdate.check_config": __plugin_implementation__.get_update_information
-	}
-
+    global __plugin_hooks__
+    __plugin_hooks__ = {
+        "octoprint.plugin5.softwareupdate.check_config": __plugin_implementation__.get_update_information
+    }
--- octoprint_filament_scale/hx711.py
+++ octoprint_filament_scale/hx711.py
@@ -1,5 +1,9 @@
 import RPi.GPIO as GPIO
 import time
+from pathlib import Path
+import json
+from datetime import datetime
+import statistics
 
 
 def bitsToBytes(a):
@@ -9,165 +13,168 @@ def bitsToBytes(a):
     for i in range(0,len(s),8):
         returnInts.append(int(s[i:i+8],2)) # goes 8 bits at a time to save as ints
     return returnInts
-	
+
+
 class HX711:
-	def __init__(self, dout, pd_sck, gain=128):
-		self.PD_SCK = pd_sck
-		self.DOUT = dout
+    def __init__(self, dout, pd_sck, gain=128):
+        self.PD_SCK = pd_sck
+        self.DOUT = dout
 
-		GPIO.setmode(GPIO.BCM)
-		GPIO.setup(self.PD_SCK, GPIO.OUT)
-		GPIO.setup(self.DOUT, GPIO.IN)
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.PD_SCK, GPIO.OUT)
+        GPIO.setup(self.DOUT, GPIO.IN)
 
-		self.GAIN = 0
-		self.REFERENCE_UNIT = 1	 # The value returned by the hx711 that corresponds to your reference unit AFTER dividing by the SCALE.
-		
-		self.OFFSET = 1
-		self.lastVal = int(0)
+        self.GAIN = 0
+        self.REFERENCE_UNIT = 1  # The value returned by the hx711 that corresponds to your reference unit AFTER dividing by the SCALE.
 
-		self.LSByte = [2, -1, -1]
-		self.MSByte = [0, 3, 1]
-		
-		self.MSBit = [0, 8, 1]
-		self.LSBit = [7, -1, -1]
+        self.OFFSET = 1
+        self.lastVal = int(0)
 
-		self.byte_format = 'LSB'
-		self.bit_format = 'MSB'
+        self.LSByte = [2, -1, -1]
+        self.MSByte = [0, 3, 1]
 
-		self.byte_range_values = self.LSByte
-		self.bit_range_values = self.MSBit
+        self.MSBit = [0, 8, 1]
+        self.LSBit = [7, -1, -1]
 
-		self.set_gain(gain)
+        self.byte_format = 'LSB'
+        self.bit_format = 'MSB'
 
-		time.sleep(1)
+        self.byte_range_values = self.LSByte
+        self.bit_range_values = self.MSBit
 
-	def is_ready(self):
-		return GPIO.input(self.DOUT) == 0
+        self.set_gain(gain)
 
-	def set_gain(self, gain):
-		if gain is 128:
-			self.GAIN = 1
-		elif gain is 64:
-			self.GAIN = 3
-		elif gain is 32:
-			self.GAIN = 2
+        time.sleep(1)
 
-		GPIO.output(self.PD_SCK, False)
-		self.read()
-	
-	def createBoolList(self, size=8):
-		ret = []
-		for i in range(size):
-			ret.append(False)
-		return ret
-	def read(self):
-		x = time.time()
-		while x+5 > time.time() and not self.is_ready():
-			GPIO.wait_for_edge(self.DOUT, GPIO.FALLING, timeout=1000)
-			time.sleep(0.01)
-		
-		dataBits = [self.createBoolList(), self.createBoolList(), self.createBoolList()]
-		dataBytes = [0x0] * 4
+    def is_ready(self):
+        return GPIO.input(self.DOUT) == 0
 
-		for j in range(self.byte_range_values[0], self.byte_range_values[1], self.byte_range_values[2]):
-			for i in range(self.bit_range_values[0], self.bit_range_values[1], self.bit_range_values[2]):
-				GPIO.output(self.PD_SCK, True)
-				
-				dataBits[j][i] = GPIO.input(self.DOUT)
-				GPIO.output(self.PD_SCK, False)
-				time.sleep(0.000001)
-				
-			dataBytes[j] = bitsToBytes(dataBits[j])[1] 
+    def set_gain(self, gain):
+        if gain is 128:
+            self.GAIN = 1
+        elif gain is 64:
+            self.GAIN = 3
+        elif gain is 32:
+            self.GAIN = 2
 
-			
-		#set channel and gain factor for next reading
-		for i in range(self.GAIN):
-			GPIO.output(self.PD_SCK, True)
-			GPIO.output(self.PD_SCK, False)
+        GPIO.output(self.PD_SCK, False)
+        self.read()
 
-		#check for all 1
-		#if all(item is True for item in dataBits[0]):
-		#	 return int(self.lastVal)
+    def createBoolList(self, size=8):
+        ret = []
+        for i in range(size):
+            ret.append(False)
+        return ret
+    def read(self):
+        x = time.time()
+        while x+5 > time.time() and not self.is_ready():
+            GPIO.wait_for_edge(self.DOUT, GPIO.FALLING, timeout=1000)
+            time.sleep(0.01)
 
-		dataBytes[2] ^= 0x80
+        dataBits = [self.createBoolList(), self.createBoolList(), self.createBoolList()]
+        dataBytes = [0x0] * 4
 
-		np_arr32 = (dataBytes[3] << 24) + (dataBytes[2] << 16) + (dataBytes[1] << 8) + dataBytes[0]
-		
-		self.lastVal = np_arr32
+        for j in range(self.byte_range_values[0], self.byte_range_values[1], self.byte_range_values[2]):
+            for i in range(self.bit_range_values[0], self.bit_range_values[1], self.bit_range_values[2]):
+                GPIO.output(self.PD_SCK, True)
 
-		return int(self.lastVal)
-		
-	def read_average(self, times=6, discard_outliers=True):
-		values = []
-		for i in range(times):
-			values.append(self.read())
+                dataBits[j][i] = GPIO.input(self.DOUT)
+                GPIO.output(self.PD_SCK, False)
+                time.sleep(0.000001)
 
-		if discard_outliers:
-			# Inspired by: https://machinelearningmastery.com/how-to-use-statistics-to-identify-outliers-in-data/
-			# Numpy felt a bit much to drag in just to clean up noisy hx711 data
-			readings_mean = statistics.mean(values)
-			readings_stdev = statistics.stdev(values)
-			cutoff = readings_stdev * 1.25
-			lower = readings_stdev - cutoff
-			upper = data_mean + cutoff
-			values = [x for x in values if x > lower and x < upper]
+            dataBytes[j] = bitsToBytes(dataBits[j])[1]
 
-		return sum(values) / len(values)
 
-	def get_raw_value(self, times=6):
-		return self.read_average(times)
+        #set channel and gain factor for next reading
+        for i in range(self.GAIN):
+            GPIO.output(self.PD_SCK, True)
+            GPIO.output(self.PD_SCK, False)
 
-	def get_weight(self, times=6):
-		value = self.read_average(times) - self.OFFSET
-		value = value / self.REFERENCE_UNIT
-		return value
+        #check for all 1
+        #if all(item is True for item in dataBits[0]):
+        #    return int(self.lastVal)
 
-	def tare(self, times=15):
-	   
-		# Backup REFERENCE_UNIT value
-		reference_unit = self.REFERENCE_UNIT
-		self.set_reference_unit(1)
+        dataBytes[2] ^= 0x80
 
-		value = self.read_average(times)
-		print(value)
-		self.set_offset(value)
+        np_arr32 = (dataBytes[3] << 24) + (dataBytes[2] << 16) + (dataBytes[1] << 8) + dataBytes[0]
 
-		self.set_reference_unit(reference_unit)
-		return value;
+        self.lastVal = np_arr32
 
-	def set_reading_format(self, byte_format="LSB", bit_format="MSB"):
+        return int(self.lastVal)
 
-		self.byte_format = byte_format
-		self.bit_format = bit_format
+    def read_average(self, times=6, discard_outliers=True):
+        values = []
+        for i in range(times):
+            values.append(self.read())
 
-		if byte_format == "LSB":
-			self.byte_range_values = self.LSByte
-		elif byte_format == "MSB":
-			self.byte_range_values = self.MSByte
+        if discard_outliers:
+            # Inspired by: https://machinelearningmastery.com/how-to-use-statistics-to-identify-outliers-in-data/
+            # Numpy felt a bit much to drag in just to clean up noisy hx711 data
+            readings_mean = statistics.mean(values)
+            readings_stdev = statistics.stdev(values)
+            cutoff = readings_stdev * 1.25
+            lower = readings_mean - cutoff
+            upper = readings_mean + cutoff
+            cleaned_values = [x for x in values if x > lower and x < upper]
+            test_data['cleaned'] = cleaned_values
+            values = cleaned_values
 
-		if bit_format == "LSB":
-			self.bit_range_values = self.LSBit
-		elif bit_format == "MSB":
-			self.bit_range_values = self.MSBit
+        return sum(values) / len(values)
 
-	def set_offset(self, offset):
-		self.OFFSET = offset
+    def get_raw_value(self, times=6):
+        return self.read_average(times)
 
-	def set_reference_unit(self, reference_unit):
-		self.REFERENCE_UNIT = reference_unit
+    def get_weight(self, times=6):
+        value = self.read_average(times) - self.OFFSET
+        value = value / self.REFERENCE_UNIT
+        return value
 
-	# HX711 datasheet states that setting the PDA_CLOCK pin on high for >60 microseconds would power off the chip.
-	# I used 100 microseconds, just in case.
-	# I've found it is good practice to reset the hx711 if it wasn't used for more than a few seconds.
-	def power_down(self):
-		GPIO.output(self.PD_SCK, False)
-		GPIO.output(self.PD_SCK, True)
-		time.sleep(0.0001)
+    def tare(self, times=15):
 
-	def power_up(self):
-		GPIO.output(self.PD_SCK, False)
-		time.sleep(0.0001)
+        # Backup REFERENCE_UNIT value
+        reference_unit = self.REFERENCE_UNIT
+        self.set_reference_unit(1)
 
-	def reset(self):
-		self.power_down()
-		self.power_up()
+        value = self.read_average(times)
+        print(value)
+        self.set_offset(value)
+
+        self.set_reference_unit(reference_unit)
+        return value;
+
+    def set_reading_format(self, byte_format="LSB", bit_format="MSB"):
+
+        self.byte_format = byte_format
+        self.bit_format = bit_format
+
+        if byte_format == "LSB":
+            self.byte_range_values = self.LSByte
+        elif byte_format == "MSB":
+            self.byte_range_values = self.MSByte
+
+        if bit_format == "LSB":
+            self.bit_range_values = self.LSBit
+        elif bit_format == "MSB":
+            self.bit_range_values = self.MSBit
+
+    def set_offset(self, offset):
+        self.OFFSET = offset
+
+    def set_reference_unit(self, reference_unit):
+        self.REFERENCE_UNIT = reference_unit
+
+    # HX711 datasheet states that setting the PDA_CLOCK pin on high for >60 microseconds would power off the chip.
+    # I used 100 microseconds, just in case.
+    # I've found it is good practice to reset the hx711 if it wasn't used for more than a few seconds.
+    def power_down(self):
+        GPIO.output(self.PD_SCK, False)
+        GPIO.output(self.PD_SCK, True)
+        time.sleep(0.0001)
+
+    def power_up(self):
+        GPIO.output(self.PD_SCK, False)
+        time.sleep(0.0001)
+
+    def reset(self):
+        self.power_down()
+        self.power_up()
