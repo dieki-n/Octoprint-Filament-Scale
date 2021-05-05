e1cafe03e702d87cc92ba77d5acbd52e5da5fbcc
Leon Wright <techman83@gmail.com>
2021-05-05 10:59:04 +0800

    test: Ensure setting defaults are present
    During refactoring a typo lead to default settings not being correct. Ensure our future defaults don't change without intent.
    
    Relates to #12

--- tests/hx711.py
+++ tests/hx711.py
@@ -1,6 +1,7 @@
 import unittest
 
 from filament_scale_enhanced.hx711 import HX711
+from filament_scale_enhanced import FilamentScalePlugin
 
 
 class TestHx711(unittest.TestCase):
@@ -11,3 +12,17 @@ class TestHx711(unittest.TestCase):
             HX711.createBoolList(4),
             bool_list
         )
+
+
+class TestFilamentScalePlugin(unittest.TestCase):
+
+    def test_settings_dict_defaults(self):
+        defaults = {
+            'tare': 8430152,
+            'reference_unit': -411,
+            'spool_weight': 200,
+            'clockpin': 21,
+            'datapin': 20,
+            'lastknownweight': 0
+        }
+        self.assertDictEqual(FilamentScalePlugin.get_settings_defaults(), defaults)
